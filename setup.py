b64b653ce45832bdf47c0cb2c876f4a09f19ed29
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-12-05 16:53:42 +0900

    setup.py 만들기

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mpvytdlp',
+    version='0.0.1',
+    description='exaclly which python-mpv + yp-dlp',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/mpvytdlp',
+    packages=find_packages(exclude=[]),
+    install_requires=[
+        'python-mpv>=1.0',
+        'yp-dlp>=2025.11.29',
+    ],
+    keywords=['mpv', 'python-mpv', 'yp-dlp', 'youtube', 'linux', 'mpvytdlp', 'pythonmpv', 'pythonmpvytdlp', 'python-mpvytdlp', 'pythonmpvyt-dlp', 'pythonmpv-ytdlp', 'python-mpvyt-dlp', 'python-mpv-ytdlp', 'pythonmpv-yt-dlp', 'python-mpv-yt-dlp', 'mpvyt-dlp', 'mpv-ytdlp', 'mpv-yt-dlp', 'mpv-yp', 'python-mpv-yt', 'mpvyt', 'ytdlp', 'pythonmpvyt', 'python-mpvyt', 'pythonmpv-yt', 'simple', 'it', 'just', 'setup', 'file', 'huh', '#wellcome to PR', 'yt', 'dlp'],
+    python_requires='>=3.6',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
